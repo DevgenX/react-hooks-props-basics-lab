7cfca8bad8de9057f0290e5a147708020dd1a298
devgenx <sebastian110420@gmail.com>
2023-01-10 09:44:43 -0500

    commit tree 8da610f62833800cdff554d46470c7736fbf450e

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,17 @@
 import React from "react";
+import Link from "./Link";
 
-function About() {
+const About = ({ user }) => {
+  const { bio, links } = user;
+
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {bio && bio.length > 1 ? <p>{bio}</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Link github={links.github} linkedin={links.linkedin}></Link>
     </div>
   );
-}
+};
 
 export default About;
--- src/components/App.js
+++ src/components/App.js
@@ -5,16 +5,15 @@ import About from "./About";
 
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
-console.log(user);
 
-function App() {
+const App = () => {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home user={user} />
+      <About user={user} />
     </div>
   );
-}
+};
 
 export default App;
