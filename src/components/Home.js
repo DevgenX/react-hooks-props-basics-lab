0b652a8f4bc8acb4feb39192591126f49c118ea3
devgenx <sebastian110420@gmail.com>
2023-01-10 09:44:55 -0500

    Destructured solution

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,14 @@
 import React from "react";
 
-function Home(props) {
+const Home = ({ user }) => {
+  const { name, color, city } = user;
   return (
     <div id="home">
-      <h1 style={{ color: props.color }}>
-        {props.name} is a Web Developer from {props.city}
+      <h1 style={{ color: color }}>
+        {name} is a Web Developer from {city}
       </h1>
     </div>
   );
-}
+};
 
 export default Home;
